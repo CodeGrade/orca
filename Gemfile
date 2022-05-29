d3ae2a0664a9885b58a86cd47c35ec8bd3fd8d18
Eli MacColl <elimaccoll@gmail.com>
2022-05-29 16:31:46 -0400

    added schema and error handling for creating grading jobs

--- Gemfile
+++ Gemfile
@@ -76,3 +76,5 @@ group :test do
   gem "selenium-webdriver"
   gem "webdrivers"
 end
+
+gem "json-schema", "~> 3.0"
--- app/controllers/api/grading_jobs_controller.rb
+++ app/controllers/api/grading_jobs_controller.rb
@@ -1,41 +1,77 @@
+require "json-schema"
+
 module Api
-    class GradingJobsController < ApiController
-        # Allows for testing 'create' without verifying CSRF token authenticity
-        protect_from_forgery except: :create
+  class GradingJobsController < ApiController
+    # Allows for testing 'create' without verifying CSRF token authenticity
+    protect_from_forgery except: :create
 
-        # Get all grading jobs
-        def index
-            grading_jobs = GradingJob.all
-            render json: grading_jobs
-        end
-
-        # Create a grading job
-        def create
-            grading_job_config = request.raw_post
-            create_grading_job(grading_job_config)
-            # TODO: Check if it succeeded
-            render json: {
-                status: "OK"
+    # TODO: Move to a different file
+    @@grading_config_schema = {
+      "type" => "object",
+      "required" => ["submission_id", "grade_id", "student_code", "priority", "script"],
+      "properties" => {
+          "submission_id" => {"type" => "integer"},
+          "grade_id" => {"type" => "integer"},
+          "student_code" => {"type" => "string"},
+          "priority" => {"type" => "integer"},
+          "script" => {"$ref" => "#/$defs/script"}
+      },
+  
+      "$defs" => {
+        "script" => {
+          "type" => "array",
+          "items" => {
+            "type" => "object",
+            "required" => ["cmd", "on_fail", "on_complete"],
+            "properties" => {
+              "cmd" => {"type" => "string"},
+              "on_fail" => {"type" => "string"},
+              "on_complete" => {"type" => "string"}
             }
-        end
+          }
+        }
+      }
+    }
 
-        def create_grading_job(grading_job_config)
-            @grading_job_config = grading_job_config
-            parsed_config = JSON.parse(@grading_job_config)
-            # TODO: Did we want config to be a string here?
-            @grading_job = GradingJob.new(
-              config: @grading_job_config, 
-              submission_id: parsed_config["submission_id"], 
-              grade_id: parsed_config["grade_id"],
-              priority: parsed_config["priority"],
-            )
-          if parsed_config["user_id"]
-            @grading_job.user_id = parsed_config["user_id"]
-          elsif parsed_config["team_id"]
-            @grading_job.team_id = parsed_config["team_id"]
-          end
-          @grading_job.save!
-          # TODO: Add job queue "enqueue" function here.
-          end
+    # Get all grading jobs
+    def index
+        grading_jobs = GradingJob.all
+        render json: grading_jobs
     end
+
+    # Create a grading job
+    def create
+      grading_job_config = request.raw_post
+      parsed_config = JSON.parse(grading_job_config)
+      unless (is_valid_grading_job_config(parsed_config))
+        return render json: {
+          errors: [{message: "Invalid grading job config."}]
+        }
+      end
+      @grading_job = GradingJob.new(
+        config: parsed_config, 
+        submission_id: parsed_config["submission_id"], 
+        grade_id: parsed_config["grade_id"],
+        priority: parsed_config["priority"],
+      )
+      if parsed_config["user_id"]
+        @grading_job.user_id = parsed_config["user_id"]
+      elsif parsed_config["team_id"]
+        @grading_job.team_id = parsed_config["team_id"]
+      end
+      unless (@grading_job.save)
+        return render json: {
+          errors: [{message: "Invalid grading job config."}]
+        }
+      end
+      # TODO: Add job queue "enqueue" function here.
+    end
+
+    private
+    def is_valid_grading_job_config(config)
+      # return JSON::Validator.validate(ValidationSchemas::GradingJobConfigSchema.schema, config)
+      return JSON::Validator.validate(@@grading_config_schema, config)
+    end
+
+  end
 end
