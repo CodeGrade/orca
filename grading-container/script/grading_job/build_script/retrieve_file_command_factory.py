45b35f85930e6caa0377020b2e40f552ff2f1865
williams-jack <williams.jack@northeastern.edu>
2022-06-02 22:44:55 -0400

    Added classes for archive retrieval commands. Redoing factory pattern.

---
+++ grading-container/script/grading_job/build_script/retrieve_file_command_factory.py
@@ -0,0 +1,8 @@
+class FileRetrievalCommandFactory():
+  """
+  Factory pattern object that, given a code file object, 
+  returns the proper concrete class of a FileRetrieval command
+  that can properly download and save (as well as possibly extract)
+  the contents of a file.
+  """
+  pass
--- grading-container/script/grading_job/build_script/retrieve_file_commands.py
+++ grading-container/script/grading_job/build_script/retrieve_file_commands.py
@@ -1,8 +1,9 @@
 from grading_job.build_script.code_file_info import CodeFileInfo 
 from grading_job.build_script.code_file_source import CodeFileSource
-from os import makedirs
+from os import makedirs, remove
 from os.path import join
 from requests import request
+import tarfile
 
 class FileRetrievalCommand:
   """
@@ -35,6 +36,10 @@ class FileRetrievalCommand:
     return self._code_file_info.get_save_dir()
 
 class SingleFileRetrievalCommand(FileRetrievalCommand):
+  """
+  Command pattern tasked with downloading and saving a single source code 
+  file.
+  """
 
   def execute(self) -> None:
     file_save_dir = self._code_file_info.get_save_dir()
@@ -46,10 +51,63 @@ class SingleFileRetrievalCommand(FileRetrievalCommand):
     file.write(file_contents)
     file.close()
 
-class ArchiveRetrievalCommand(FileRetrievalCommand):
+class ArchiveFileRetrievalCommand(FileRetrievalCommand):
+  """
+  Command pattern tasked with downloading an archived file and extracting 
+  its contents into the proper folder.
+  """
 
   def execute(self) -> None:
     # 1. Download and save file to directory.
+    file_name = self._code_file_info.get_file_name()
+    file_save_dir = self._code_file_info.get_save_dir()
+    makedirs(file_save_dir)
+    file_path = join(file_save_dir, file_name)
+    file_contents = self._get_file_contents()
+    file = open(file_path, "w")
+    file.write(file_contents)
+    file.close()
     # 2. Extract file based on MIME type.
+    self._extract_files(file_path, file_save_dir)
     # 3. Remove original archive/compressed file.
+    remove(file_path)
+
+  def _extract_files(self, acv_file_path: str, extract_dir: str) -> None:
+    """
+    Abstract method for extract some number of files from a fiven
+    archive/compressed file based on type.
+    """
     pass
+
+
+class TarFileRetrievalCommand(ArchiveFileRetrievalCommand):
+  """
+  Retrieval command that downloads, saves, and extracts the contents
+  from a .tar file.
+  """
+
+  def _extract_files(self, acv_file_path: str, extract_dir: str) -> None:
+    tf = tarfile.open(acv_file_path, "r")
+    tf.extractall(extract_dir)
+  
+
+class ZipFileRetrievalCommand(ArchiveFileRetrievalCommand):
+  """
+  Retrival command that downloads, saves, and extracts the contents from
+  a .zip file.
+  """
+  pass
+
+class GZipFileRetrievalCommnad(ArchiveFileRetrievalCommand):
+  """
+  Retrieval command that downloads, saves, and extracts the contents 
+  from a .gz file.
+  """
+  pass
+
+class TarGZFileRetrievalCommand(ArchiveFileRetrievalCommand):
+  """
+  Retrieval command that downloads, saves, and extracts the contents
+  from a .tgz/.tar.gz file.
+  """
+  pass
