3bfe489f26b4e042c5ccc39671f53afa86446b37
Eli MacColl <elimaccoll@gmail.com>
2022-11-14 14:25:00 -0500

    refactor: move orca.tsx logic into App.tsx

--- web-client/src/App.tsx
+++ web-client/src/App.tsx
@@ -1,13 +1,21 @@
+import React from "react";
 import "./App.css";
 import "./vendors/bootswatch/lux/bootstrap.min.css";
-import Orca from "./components/orca";
-import React from "react";
+import { Provider } from "react-redux";
+import OrcaNavbar from "./components/navbar/navbar";
+import Dashboard from "./components/dashboard/dashboard";
+import gradingJobReducer from "./components/reducers/grading-job-reducer";
+import { configureStore } from "@reduxjs/toolkit";
+
+const reducer = gradingJobReducer;
+const store = configureStore({ reducer });
 
 const App = () => {
   return (
-    <div>
-      <Orca />
-    </div>
+    <Provider store={store}>
+      <OrcaNavbar />
+      <Dashboard />
+    </Provider>
   );
 };
 
