8e1cc16ba742d3ddc7893de2f29df13a493581b8
williams-jack <williams.jack@northeastern.edu>
2022-12-24 19:40:12 -0500

    feat: reasonable coverage of GradingScriptCommand subclasses

--- grading-vm/orca_grader/tests/container/grading_script/test_bash_grading_script_command.py
+++ grading-vm/orca_grader/tests/container/grading_script/test_bash_grading_script_command.py
@@ -58,3 +58,39 @@ class TestBashGradingScriptCommand(unittest.TestCase):
     self.assertEqual(response.get_stderr_output(), "")
     self.assertNotEqual(response.get_stdout_output(), "")
     self.assertTrue(response.did_time_out)
+
+  def test_bash_command_traverses_on_fail(self):
+    expected_tap = "this should be reached upon first failure"
+    second_cmd = BashGradingScriptCommand(f"echo {expected_tap}", 1)
+    first_cmd = BashGradingScriptCommand("(exit 1)", 1, on_fail=second_cmd)
+
+    output = first_cmd.execute([])
+    responses = output.get_command_responses()
+    exceptions = output.get_execution_errors()
+    tap_output = output.get_tap_output()
+
+    self.assertEqual(exceptions, [])
+    self.assertEqual(tap_output, expected_tap)
+    
+    self.assertEqual(len(responses), 2)
+    for response in responses:
+      self.assertEqual(response.get_stderr_output(), "")
+    self.assertEqual(responses[1].get_stdout_output(), expected_tap)
+
+  def test_bash_command_traverses_on_complete(self):
+    expected_tap = "this should be reached from first on complete"
+    first_cmd_output = "command one completed"
+    second_cmd = BashGradingScriptCommand(f"echo {expected_tap}", 1)
+    first_cmd = BashGradingScriptCommand(f"echo {first_cmd_output}", 1, on_complete=second_cmd)
+
+    output = first_cmd.execute([])
+    responses, exceptions, tap_output = output.get_command_responses(), \
+        output.get_execution_errors(), output.get_tap_output()
+
+    self.assertEqual(len(exceptions), 0)
+    self.assertEqual(tap_output, expected_tap)
+
+    for response in responses:
+      self.assertEqual(response.get_stderr_output(), "")
+    self.assertEqual(responses[0].get_stdout_output(), first_cmd_output)
+    self.assertEqual(responses[1].get_stdout_output(), expected_tap)
--- grading-vm/orca_grader/tests/runner.py
+++ grading-vm/orca_grader/tests/runner.py
@@ -3,6 +3,7 @@ import unittest
 # Import test case modules
 import orca_grader.tests.container.build_script.test_cycle_detector as test_cycle_detector
 import orca_grader.tests.container.grading_script.test_bash_grading_script_command as test_bash_grading_script_command
+import orca_grader.tests.container.grading_script.test_conditional_grading_script_command as test_conditional_grading_script_command
 
 if __name__ == '__main__':
   loader = unittest.TestLoader()
@@ -10,5 +11,6 @@ if __name__ == '__main__':
   # Load Test Cases
   suite.addTests(loader.loadTestsFromModule(test_cycle_detector))
   suite.addTests(loader.loadTestsFromModule(test_bash_grading_script_command))
+  suite.addTests(loader.loadTestsFromModule(test_conditional_grading_script_command))
   runner = unittest.TextTestRunner(verbosity=3)
   runner.run(suite)
