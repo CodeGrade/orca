88c7fa72acf543f41ff4ba912d804f1799879d52
williams-jack <williams.jack@northeastern.edu>
2023-06-15 21:46:49 -0400

    feat: js echo server with Dockerfile

---
+++ grading-vm/images/testing/echo-server/index.js
@@ -0,0 +1,33 @@
+import express, { json } from "express";
+
+const PORT = 9001;
+
+const app = express();
+app.use(json());
+
+const responses = {};
+
+app.get("/", (_, res) => {
+  res.send("Hello world!");
+});
+
+app.get("/job-output/:key", (req, res) => {
+  const { key } = req.params;
+  if (responses[key]) {
+    return res.json(responses[key]);
+  }
+  return res.sendStatus(400);
+});
+
+app.post("/job-output", (req, res) => {
+  const { key, output } = req.body;
+  if (!!key && !!output) {
+    responses[key] = output;
+    return res.sendStatus(200);
+  }
+  return res.sendStatus(400);
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}.`);
+});
