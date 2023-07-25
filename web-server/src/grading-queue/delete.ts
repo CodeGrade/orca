503b53663b04b1a186dc611d130d34e3a5434c35
williams-jack <williams.jack@northeastern.edu>
2023-07-25 09:59:51 -0400

    refactor: removeNonImmediate only relevant for upgrade

--- web-server/src/grading-queue/delete.ts
+++ web-server/src/grading-queue/delete.ts
@@ -1,15 +1,6 @@
 import { RedisClientType } from "redis";
-import {
-  Collation,
-  CollationType,
-  DeleteJobRequest,
-  EnrichedGradingJob,
-} from "./types";
-import {
-  AssertionError,
-  JobNonexistentError,
-  NonImmediateJobRemovalError,
-} from "./exceptions";
+import { CollationType, DeleteJobRequest } from "./types";
+import { JobNonexistentError } from "./exceptions";
 import assert from "assert";
 
 const deleteJob = async (
@@ -40,41 +31,5 @@ const deleteJob = async (
 
   await client.DEL(jobKey);
 };
-
-export const removeNonImmediateJob = async (
-  client: RedisClientType,
-  jobKey: string,
-  collation: Collation,
-): Promise<EnrichedGradingJob> => {
-  const collationKey = `${collation.type}.${collation.id}`;
-  const submitterInfoKey = `SubmitterInfo.${collationKey}`;
-  const noncesKey = `Nonces.${collationKey}`;
-
-  const submitterKeyPos = await client.LPOS(submitterInfoKey, jobKey);
-  if (submitterKeyPos === null) {
-    throw new NonImmediateJobRemovalError(
-      `The job key ${jobKey} was not found in ${submitterInfoKey}.`,
-    );
-  }
-
-  const [nonce] = await client.SPOP(noncesKey);
-
-  const reservationKey = `${collationKey}.${nonce}`;
-  await client.ZREM("Reservations", reservationKey);
-
-  await client.LREM(submitterInfoKey, 1, jobKey);
-
-  const deletedJob = await client.GETDEL(jobKey);
-  assertDeletedKey(deletedJob);
-
-  return JSON.parse(deletedJob) as EnrichedGradingJob;
-};
-
-function assertDeletedKey(key: string | null): asserts key is string {
-  if (typeof key !== "string")
-    throw new AssertionError(
-      "Did not successfully remove job key after removing it's queue references.",
-    );
-}
 
 export default deleteJob;
