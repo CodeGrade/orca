6bee2176081eea30a3cf3a179bb6be81388bd4e8
williams-jack <williams.jack@northeastern.edu>
2023-10-22 21:26:47 -0400

    chore: update init with network and .env defaults

--- grading-vm/init_dev.sh
+++ grading-vm/init_dev.sh
@@ -10,11 +10,12 @@ python -m pip install -r requirements.txt
 
 if [ ! -f ".env" ]; then
   echo "Creating .env from .default.env..."
-  cp .default.env .env
+  cp .env.defaults .env
   echo "Created."
   echo "Please make any desired environment variable changes in .env."
 fi
 
+docker network create orca-testing
 docker build -t orca-grader-base -f images/orca-grader-base.Dockerfile .
 docker build -t orca-java-grader -f images/java-grader.Dockerfile .
 docker save orca-java-grader -o images/testing/simple-server/files/images/orca-java-grader.tgz
