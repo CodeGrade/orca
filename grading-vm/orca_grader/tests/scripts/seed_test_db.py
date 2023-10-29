355e8af07c45fa7daef140a6a3f76da1a14bab7b
williams-jack <williams.jack@northeastern.edu>
2023-10-29 19:04:22 -0400

    chore: add ability to populate basic jobs

--- grading-vm/orca_grader/tests/scripts/seed_test_db.py
+++ grading-vm/orca_grader/tests/scripts/seed_test_db.py
@@ -1,13 +1,34 @@
+from base64 import b64encode
 import json
 import sys
-from orca_grader.common.types.grading_job_json_types import GradingJobJSON
 from redis import Redis
 import time
-
+import os
+from copy import copy
+from orca_grader import get_redis_client
+from orca_grader.common.types.grading_job_json_types import GradingJobJSON
 from orca_grader.config import APP_CONFIG
-from orca_grader.queue_diagnostics import JobState, log_diagnostics
 
 ONE_DAY = 60 * 60 * 24 # secs * mins * hours
+
+__BASIC_GRADING_JOB_SCAFFOLDING = {
+  'script': [
+    {
+      'cmd': ['echo', 'This is a basic command.']
+    },
+    {
+      'cmd': ['echo', 'This is some output.'],
+      'on_complete': 'output'
+    }
+  ],
+  'grader_image_sha': 'orca-java-grader',
+  'collation': {
+    'type': 'user'
+  },
+  'response_url': 'http://echo-server:9001/job-output',
+  'files': {},
+  'metadata_table': {}
+}
 
 def add_job_to_queue(client: Redis, job_json: GradingJobJSON) -> None:
   key, collation, priority = job_json["key"], job_json["collation"], job_json["priority"]
@@ -23,9 +44,11 @@ def add_job_to_queue(client: Redis, job_json: GradingJobJSON) -> None:
   #   log_diagnostics(client, arrival_time, key, JobState.CREATED)
   #   log_diagnostics(client, nonce, key, JobState.RELEASED)
 
-if __name__ == "__main__":
-  _, json_path = sys.argv
-  with open(json_path, 'r') as json_fp:
-    job_json = json.load(json_fp)
-  client = Redis.from_url(APP_CONFIG.redis_db_url)
-  add_job_to_queue(client, job_json, '1')
+if __name__ == '__main__':
+  for i in range(50):
+    job = copy(__BASIC_GRADING_JOB_SCAFFOLDING)
+    job['key'] = str(i)
+    job['collation']['id'] = str(i)
+    job['priority'] = i * 60
+    client = get_redis_client(APP_CONFIG.redis_db_url)
+    add_job_to_queue(client, job)
