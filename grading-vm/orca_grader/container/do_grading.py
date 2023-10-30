6ec30538933dc56ca89a2e9ef13caf3b502506a6
williams-jack <williams.jack@northeastern.edu>
2023-10-30 15:58:07 -0400

    fix: missing logic in second use of push results func

--- grading-vm/orca_grader/container/do_grading.py
+++ grading-vm/orca_grader/container/do_grading.py
@@ -51,7 +51,8 @@ def do_grading(secret: str, grading_job_json: GradingJobJSON) -> GradingJobResul
     output = GradingJobResult(command_responses, [preprocess_e])
   except Exception as e:
     output = GradingJobResult(command_responses, [e])
-  push_results_to_response_url(output, grading_job_json["key"],
+  push_results_to_response_url(output,
+                               grading_job_json["key"],
                                grading_job_json["container_response_url"] if \
                                 "container_response_url" in grading_job_json else \
                                   grading_job_json["response_url"])
@@ -67,6 +68,10 @@ if __name__ == "__main__":
   except Exception as e:
     traceback.print_tb(e.__traceback__)
     output = GradingJobResult([], [e.with_traceback(None)])
-    push_results_to_response_url(output, grading_job["key"], grading_job["response_url"])
+    push_results_to_response_url(output,
+                                 grading_job["key"],
+                                 grading_job["container_response_url"] if \
+                                  "container_response_url" in grading_job else \
+                                    grading_job["response_url"])
   # cleanup(secret) # useful for execution with no container, but generally optional
 
