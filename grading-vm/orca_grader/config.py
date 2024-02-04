cc889e300a8892f68e7a77e917906dd24b9c0f5b
williams-jack <williams.jack@northeastern.edu>
2024-02-04 16:29:19 -0500

    chore: worker config defaults to orchestrator for images
    Previously the orchestrator functionality had not been set
    up to serve images, and the simple server container was used instead.

--- grading-vm/orca_grader/config.py
+++ grading-vm/orca_grader/config.py
@@ -4,9 +4,8 @@ class AppConfig():
 
   def __init__(self) -> None:
     dotenv.load_dotenv()
-    self.orca_web_server_host = os.getenv('ORCA_WEB_SERVER_HOST', 'http://localhost:9000')
+    self.orca_web_server_host = os.getenv('ORCA_WEB_SERVER_HOST', 'http://localhost:4000')
     self.redis_db_url = os.getenv('REDIS_DB_URL', 'redis://localhost:6379')
-    self.bottlenose_host = os.getenv('BOTTLENOSE_HOST', 'http://localhost:3000')
     __enable_diagnostics_val = os.getenv('ENABLE_DIAGNOSTICS') or ""
     self.enable_diagnostics =  __enable_diagnostics_val.lower() == "true"
 
