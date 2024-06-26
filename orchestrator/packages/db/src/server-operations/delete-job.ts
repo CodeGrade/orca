78cc8a4ee0ab857a5e5ba13dda9cdf25c2770a81
williams-jack <williams.jack@northeastern.edu>
2024-06-26 19:18:45 -0400

    feat: notify client of cancelled job

--- orchestrator/packages/db/src/server-operations/delete-job.ts
+++ orchestrator/packages/db/src/server-operations/delete-job.ts
@@ -2,7 +2,7 @@ import { getAssociatedReservation, retireReservationAndJob } from "../utils";
 import prismaInstance from "../prisma-instance";
 import { Job } from "@prisma/client";
 
-const deleteJob = (jobID: number) =>
+const deleteJob = (jobID: number): Promise<Job> =>
   prismaInstance.$transaction(async (tx) => {
     const job = await tx.job.findUnique({
       where: {
@@ -11,6 +11,7 @@ const deleteJob = (jobID: number) =>
     }) as Job;
     const reservation = await getAssociatedReservation(job, tx);
     await retireReservationAndJob(job, reservation, tx);
+    return job;
   });
 
 export default deleteJob;
