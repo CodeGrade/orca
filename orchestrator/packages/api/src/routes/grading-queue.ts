f709fd5030c07993c6a18c01e28967780bb7fd55
williams-jack <williams.jack@northeastern.edu>
2024-07-04 15:12:12 -0400

    fix: missing router connection to controller func

--- orchestrator/packages/api/src/routes/grading-queue.ts
+++ orchestrator/packages/api/src/routes/grading-queue.ts
@@ -5,6 +5,7 @@ import {
   moveJob,
   deleteJob,
   createOrUpdateImmediateJob,
+  getJobStatus,
 } from "../controllers/grading-queue-controller";
 
 const gradingQueueRouter = Router();
@@ -15,5 +16,6 @@ gradingQueueRouter.put("/grading_queue", createOrUpdateJob);
 gradingQueueRouter.put("/grading_queue/immediate", createOrUpdateImmediateJob);
 gradingQueueRouter.put("/grading_queue/move", moveJob);
 gradingQueueRouter.delete("/grading_queue/:jobID", deleteJob);
+gradingQueueRouter.post("/grading_queue/job_status", getJobStatus);
 
 export default gradingQueueRouter;
