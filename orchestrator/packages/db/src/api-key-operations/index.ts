f127ec8dc887a4a05389af15bb5484791015ec07
williams-jack <williams.jack@northeastern.edu>
2024-08-05 13:29:30 -0400

    fix: random bytes string twice the size of expected

--- orchestrator/packages/db/src/api-key-operations/index.ts
+++ orchestrator/packages/db/src/api-key-operations/index.ts
@@ -18,4 +18,6 @@ export const createAPIKey = async (hostname: string): Promise<string> =>
 export const validAPIKey = async (hostname: string, value: string) =>
   Boolean(await prismaInstance.apiKey.count({ where: { hostname, value } }))
 
-const generateKey = (): string => randomBytes(KEY_LENGTH).toString('hex');
+// randomBytes into hex string will return a value with double the length passed
+// to randomBytes
+const generateKey = (): string => randomBytes(KEY_LENGTH / 2).toString('hex');
