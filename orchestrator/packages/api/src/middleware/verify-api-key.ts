9f0e7df12b67b1109560fd79da704af5f6e5e4f6
williams-jack <williams.jack@northeastern.edu>
2024-08-20 17:52:27 -0400

    chore: add logging for api key validation

--- orchestrator/packages/api/src/middleware/verify-api-key.ts
+++ orchestrator/packages/api/src/middleware/verify-api-key.ts
@@ -1,8 +1,10 @@
+import { logger } from "@codegrade-orca/common";
 import { validAPIKey } from "@codegrade-orca/db";
 import { Request, Response, NextFunction } from "express";
 
 const verifyAPIKey = async (req: Request, res: Response, next: NextFunction) => {
   const apiKey = req.header("x-api-key");
+  logger.info(`API Key |${apiKey || 'None'}| Hostname |${req.hostname}|.`);
   if (!apiKey) {
     return res.sendStatus(401);
   }
