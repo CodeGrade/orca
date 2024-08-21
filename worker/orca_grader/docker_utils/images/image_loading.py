bf6ee0e66b036538eb7d706a232f059c85c8d3aa
williams-jack <williams.jack@northeastern.edu>
2024-08-21 17:40:34 -0400

    fix: update log statements for more info on image download/import

--- worker/orca_grader/docker_utils/images/image_loading.py
+++ worker/orca_grader/docker_utils/images/image_loading.py
@@ -9,7 +9,7 @@ _LOGGER = logging.getLogger(__name__)
 
 def retrieve_image_tgz_from_url(container_sha: str, images_url: str) -> None:
     file_name = "{0}.tgz".format(container_sha)
-    _LOGGER.debug(f"Attempting to download image from {images_url}/{file_name}")
+    _LOGGER.debug(f"Attempting to download image from {images_url}")
     download_file(images_url, file_name)
     _LOGGER.debug("Image downloaded.")
 
@@ -22,9 +22,9 @@ def load_image_from_tgz(tgz_file_path: str) -> None:
         tgz_file_path
     ]
     _LOGGER.debug(f"Attempting to load image from file {tgz_file_path}")
-    subprocess.run(program_args, stdout=subprocess.DEVNULL,
-                   stderr=subprocess.STDOUT,
-                   check=True)
+    result = subprocess.run(program_args, check=True, capture_output=True)
+    _LOGGER.debug(f"Docker save stdout: {result.stdout.decode() if result.stdout is not None else '<None>' }")
+    _LOGGER.debug(f"Docker save stderr: {result.stderr.decode() if result.stderr is not None else '<None>' }")
     _LOGGER.debug("Image loaded.")
-    os.remove(tgz_file_path)  # To save resources, clean up tgz after load.
-    _LOGGER.debug("Image tgz file removed.")
+    # os.remove(tgz_file_path)  # To save resources, clean up tgz after load.
+    # _LOGGER.debug("Image tgz file removed.")
