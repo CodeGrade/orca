6a8ecb1579993047ff4663f7ca66ec799e225b0b
williams-jack <williams.jack@northeastern.edu>
2024-08-21 16:04:56 -0400

    fix: import error

--- orchestrator/packages/api/src/controllers/docker-image-controller.ts
+++ orchestrator/packages/api/src/controllers/docker-image-controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { errorResponse, formatValidationError, formatValidationErrors } from "./utils";
+import { errorResponse, formatValidationErrors } from "./utils";
 import {
     graderImageExists,
   logger,
