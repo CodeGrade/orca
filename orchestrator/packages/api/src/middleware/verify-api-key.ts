b6ab143ff7aba03d765beca6382badabae63fc20
williams-jack <williams.jack@northeastern.edu>
2024-08-21 10:30:50 -0400

    fix: only utilize url hostname handler in prod

--- orchestrator/packages/api/src/middleware/verify-api-key.ts
+++ orchestrator/packages/api/src/middleware/verify-api-key.ts
@@ -1,22 +1,23 @@
-import { logger } from "@codegrade-orca/common";
+import { getConfig, logger } from "@codegrade-orca/common";
 import { validAPIKey } from "@codegrade-orca/db";
 import { Request, Response, NextFunction } from "express";
 
 const verifyAPIKey = async (req: Request, res: Response, next: NextFunction) => {
   const apiKey = req.header("x-api-key");
-  const urlStr = req.body.response_url ?? req.header;
-  logger.info(`API Key: |${apiKey ?? 'None'}| URLString: |${urlStr ?? 'None'}|`);
-  if (!apiKey) {
-    return res.sendStatus(401);
-  }
+  const urlStr = req.body['response_url'] ?? req.header('x-forwarded-for');
   try {
-    const url = new URL(urlStr);
-    if (await validAPIKey(url.hostname, apiKey)) {
-      return next();
+    const hostname = getConfig().environment === 'production' ? hostnameHandler(urlStr) : req.hostname;
+    logger.info(`API Key: |${apiKey ?? 'None'}| Hostname: |${hostname}|`);
+    if (!apiKey) {
+      return res.sendStatus(401);
     }
+    return (await validAPIKey(hostname, apiKey)) ? next() : res.sendStatus(401);
   } catch (e) {
     logger.warn(`Error: while constructing url or validating key for ${apiKey} and ${urlStr}: ${e}`);
+    res.sendStatus(401);
   }
-  res.sendStatus(401);
 };
+
+const hostnameHandler = (urlStr: string) => new URL(urlStr).hostname;
+
 export default verifyAPIKey;
