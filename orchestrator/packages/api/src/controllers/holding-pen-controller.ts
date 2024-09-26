90610d5feaf00fd85699fdd9438746ce6a8d3216
Ben Lerner <blerner@ccs.neu.edu>
2024-09-26 12:19:43 -0400

    typo

--- orchestrator/packages/api/src/controllers/holding-pen-controller.ts
+++ orchestrator/packages/api/src/controllers/holding-pen-controller.ts
@@ -8,7 +8,7 @@ export const holdingPenStatus = async (req: Request, res: Response) => {
     return errorResponse(res, 400, [`Given job config ID ${jobConfigID} is not a number.`]);
   }
   if (await jobConfigInHoldingPen(jobConfigID)) {
-    res.json("This job is waiting of a grader image to build.");
+    res.json("This job is waiting on a grader image to build.");
   } else {
     res.json("This job could not be found in a holding pen. Please contact an admin or professor.");
   }
