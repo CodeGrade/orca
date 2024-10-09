3bab8f668fdb2f8b9de5d534eef01a50850ebee7
williams-jack <williams.jack@northeastern.edu>
2024-10-09 15:39:49 -0400

    fix: throw error on touching grader image if it doesn't exist

--- orchestrator/packages/api/src/utils/grader-images.ts
+++ orchestrator/packages/api/src/utils/grader-images.ts
@@ -1,16 +1,27 @@
 import { getConfig, GradingJobConfig, logger } from "@codegrade-orca/common";
 import { execFile } from "child_process";
+import { existsSync } from "fs";
 import path = require("path");
 
 const CONFIG = getConfig();
+
+export class TouchGraderImageFileError extends Error {
+}
 
 export const touchGraderImageFile = ({
   grader_image_sha,
 }: GradingJobConfig): Promise<void> => {
   return new Promise((resolve, reject) => {
+    const tgzFileName = `${grader_image_sha}.tgz`;
+    const tgzPath = path.join(CONFIG.dockerImageFolder, tgzFileName);
+    if (!existsSync(tgzPath)) {
+      // We do not want to _create_ the path if it does not exist already,
+      // throwing off other logic that relies on its existence.
+      return reject(new TouchGraderImageFileError(`No grader image exists with name ${tgzFileName}.`));
+    }
     execFile(
       "touch",
-      [path.join(CONFIG.dockerImageFolder, `${grader_image_sha}.tgz`)],
+      [tgzPath],
       (err, _stdout, _stderr) => {
         if (err) {
           reject(err);
